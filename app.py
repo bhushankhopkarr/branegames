6791d4d01a7a778cc2482b7a38f3274e7ee00e94
craniacshencil <rajnikamrajnikam@gmail.com>
2024-10-02 23:29:01 +0530

    Add extra 2 models

--- app.py
+++ app.py
@@ -20,8 +20,10 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 # Load your trained model
 scratch_model = load_model("models/scratch.keras")
-base_model = load_model("models/resnet-basic.keras")
-tuned_model = load_model("models/resnet-tuned.keras")
+resnet_model = load_model("models/resnet-basic.keras")
+resnet_tuned_model = load_model("models/resnet-tuned.keras")
+vgg_model = load_model("models/vgg16_final_model.keras")
+effnet_model = load_model("models/effnet.keras")
 
 # Assuming your model has a property for class names
 class_names = ("glioma", "meningioma", "notumor", "pituitary")
@@ -44,48 +46,63 @@ def prepare_image(image_path):
     img = Image.open(image_path)
     # Resize to match the model input shape (150x150)
     img = img.resize((128, 128))
-    img_array = np.array(img) / 255.0  # Normalize the image
-    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+    img_array = np.expand_dims(np.array(img), axis=0)  # Add batch dimension
     return img_array
+
+
+def prepare_effnet(image_path):
+    img = Image.open(image_path)
+    # Resize to match the model input shape (150x150)
+    img = img.resize((150, 150))
+    img_array = np.expand_dims(np.array(img), axis=0)  # Add batch dimension
+    return img_array
+
+
+def predict_tumor(model, img, name):
+    prediction = model.predict(img)
+    predicted_class = np.argmax(
+        prediction, axis=1
+    )  # Get the index of the predicted class
+    confidence = round(np.max(prediction) * 100, 2)
+    if name == "Model from scratch" or name == "VGG16":
+        prediction = softmax(prediction)
+        confidence = round(np.max(prediction) * 100, 2)
+    predicted_label = class_names[predicted_class[0]]
+    flash(
+        f"""{name}: {predicted_label} \t|\tConfidence: {
+            confidence}%""",
+        "success",
+    )
+
+
+def softmax(x):
+    exp_x = np.exp(x - np.max(x))
+    return exp_x / exp_x.sum(axis=1, keepdims=True)
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    form = UploadForm()  # Create an instance of the form
-    if form.validate_on_submit():  # Check if form is submitted and valid
-        file = form.file.data  # Get the uploaded file
+    form = UploadForm()
+    if form.validate_on_submit():
+        file = form.file.data
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
             file.save(file_path)
 
-            # Prepare the image and make a prediction
             img_array = prepare_image(file_path)
+            img_array2 = prepare_effnet(file_path)
 
-            scratch_prediction = scratch_model.predict(img_array)
-            print("scratch prediction:", scratch_prediction)  # Debugging line
-            predicted_class = np.argmax(
-                scratch_prediction, axis=1
-            )  # Get the index of the predicted class
-            scratch_predicted_label = class_names[predicted_class[0]]
+            predict_tumor(scratch_model, img_array, "Model from scratch")
 
-            base_prediction = base_model.predict(img_array)
-            print("base prediction:", base_prediction)  # Debugging line
-            predicted_class = np.argmax(
-                base_prediction, axis=1
-            )  # Get the index of the predicted class
-            base_predicted_label = class_names[predicted_class[0]]
+            predict_tumor(resnet_model, img_array, "ResNet")
 
-            tuned_prediction = tuned_model.predict(img_array)
-            print("tuned prediction:", tuned_prediction)  # Debugging line
-            predicted_class = np.argmax(
-                tuned_prediction, axis=1
-            )  # Get the index of the predicted class
-            tuned_predicted_label = class_names[predicted_class[0]]
+            predict_tumor(resnet_tuned_model, img_array, "ResNet Tuned")
 
-            flash(f"Scratch Prediction: {scratch_predicted_label}", "success")
-            flash(f"Base Prediction: {base_predicted_label}", "success")
-            flash(f"Tuned Prediction: {tuned_predicted_label}", "success")
+            predict_tumor(effnet_model, img_array2, "Effnet")
+
+            predict_tumor(vgg_model, img_array, "VGG16")
+
             return redirect(url_for("index"))
         else:
             flash("File not allowed or invalid format", "danger")
